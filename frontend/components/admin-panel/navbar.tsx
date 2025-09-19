b7bcd24cba7940436016285f91c3a964f7f4223a
Solusiit-2025 <admin-user@solusiit.id>
2025-09-19 15:33:51 +0700

    Update Z-Index

--- frontend/components/admin-panel/navbar.tsx
+++ frontend/components/admin-panel/navbar.tsx
@@ -10,7 +10,7 @@ interface NavbarProps {
 
 export function Navbar({ title, role }: NavbarProps) {
   return (
-    <header className="sticky top-0 z-50 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
+    <header className="sticky top-0 z-50 md:z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-20 items-center">
         <div className="flex items-center space-x-4 lg:space-x-0">
           <SheetMenu role={role} />
