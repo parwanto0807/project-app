e80a8e62e6122a4740bb1315bbb36a34a2b31213
Solusiit-2025 <admin-user@solusiit.id>
2025-11-01 02:58:09 +0700

    Update Sales Order Table

--- frontend/components/sales/salesOrder/tabelData.tsx
+++ frontend/components/sales/salesOrder/tabelData.tsx
@@ -791,15 +791,15 @@ function MobileSalesOrderCard({ order, onExpand, onDeleteSuccess, role }: { orde
                         onClick={() => router.push(`/admin-area/sales/quotation/create/${order.id}`)}
                         className="cursor-pointer hover:bg-cyan-700 dark:hover:text-white"
                     >
-                        + PH.
+                        ➕PH.
                     </Button>
                     <Button
                         variant="default"
                         size="sm"
                         onClick={() => router.push(`/admin-area/logistic/spk/create/${order.id}`)}
-                        className="cursor-pointer hover:bg-cyan-700 dark:hover:text-white ml-4"
+                        className="cursor-pointer hover:bg-cyan-700 dark:hover:text-white"
                     >
-                        + SPK
+                        ➕SPK
                     </Button>
 
                     <Button
