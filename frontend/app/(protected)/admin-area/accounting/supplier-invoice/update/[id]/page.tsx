11f66ad7699bd05ede7fc9305660e5e08cd05261
Solusiit-2025 <admin-user@solusiit.id>
2026-07-29 10:08:49 +0700

    Update Ndlogok Asu

--- frontend/app/(protected)/admin-area/accounting/supplier-invoice/update/[id]/page.tsx
+++ frontend/app/(protected)/admin-area/accounting/supplier-invoice/update/[id]/page.tsx
@@ -17,7 +17,7 @@ import { AdminLayout } from "@/components/admin-panel/admin-layout";
 export default async function UpdateSupplierInvoicePage({
     params,
 }: {
-    params: { id: string };
+    params: Promise<{ id: string }>;
 }) {
     const user = await getUserFromToken();
 
@@ -29,7 +29,7 @@ export default async function UpdateSupplierInvoicePage({
         redirect("/unauthorized");
     }
 
-    const { id } = params;
+    const { id } = await params;
     const response = await getSupplierInvoiceById(id);
 
     if (!response.success || !response.data) {
